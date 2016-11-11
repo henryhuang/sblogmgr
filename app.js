289e63b9984040baa4504ca5282b471933ac03d6
yijhuang <yijhuang@ebay.com>
2016-11-12 00:47:18 +0800

    config address

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
+const config = require('./cfg/config.json')
 
 
-// finally create server
-require('./lib/server').start();
+// finally start server
+require('./lib/server').start(config.publicPath, config.server.address, config.server.port);
