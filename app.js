9766ffa7d5f77eb5c8a52524b7217d5a2da56e04
yijhuang <yijhuang@ebay.com>
2016-11-12 13:14:28 +0800

    use express

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const config = require('./cfg/config.json')
 
 
 // finally start server
-require('./lib/server').start(config.publicPath, config.server.address, config.server.port);
+require('./lib/server').startServer(config);
